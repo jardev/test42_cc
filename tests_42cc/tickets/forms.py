2655e4f554fc0bb938787615653ffcef86d7892e
jardev <jardev@gmail.com>
2010-06-16 12:38:59 +0300

    ticket6: Added calendar widget to agent's form

--- tests_42cc/tickets/forms.py
+++ tests_42cc/tickets/forms.py
@@ -1,11 +1,15 @@
 from django import forms
 from django.forms.models import inlineformset_factory
+from django.contrib.admin import widgets
 from tests_42cc.tickets.models import Agent, ContactInfo
 
 class AgentForm(forms.ModelForm):
     class Meta:
         model = Agent
         fields = ('first_name', 'last_name', 'biography', 'birthday',)
+        widgets = {
+            'birthday' : widgets.AdminDateWidget()
+        }
         
 ContactFormSet = inlineformset_factory(Agent, ContactInfo, can_delete=True)        
         
