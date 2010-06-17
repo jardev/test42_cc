a83d7bd95c5c3812f2a15cff3c4b2c01b3d7cafc
jardev <jardev@gmail.com>
2010-06-17 20:37:07 +0300

    ticket:5 Switched to sqlite, added admin to fixtures

--- tests_42cc/settings.py
+++ tests_42cc/settings.py
@@ -15,10 +15,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'tests_42cc',                      # Or path to database file if using sqlite3.
-        'USER': 'tests_42cc',                      # Not used with sqlite3.
-        'PASSWORD': 'qweasd1',                  # Not used with sqlite3.
+        'ENGINE': 'sqlite3',             # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'tests_42cc.db',         # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
