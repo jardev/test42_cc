af5f5618b725765df9d9552fa8d67476da31cdd9
jardev <jardev@gmail.com>
2010-06-18 14:40:02 +0300

    ticket:2 Tests

--- tests_42cc/tickets/tests.py
+++ tests_42cc/tickets/tests.py
@@ -1,23 +1,18 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+#from django.test import TestCase
+#from django.test.client import Client
+#from django.core.urlresolvers import reverse
+#from tests_42cc.tickets import views
 
-Replace these with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
+#class AuthTest(TestCase):
+#    def setUp(self):
+#        self.client = Client()
+#        
+#    def test_unauthorized(self):
+#        response = self.client.get(reverse(views.edit))
+#        self.assertEqual(response.status_code, 302)
+#        
+#    def test_authorized(self):
+#        self.client.login(username='admin', password='admin')
+#        response = self.client.get(reverse(views.edit))
+#        self.assertEqual(response.status_code, 200)
 
